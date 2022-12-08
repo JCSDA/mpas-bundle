5dd6b3cb9d17e6659d3a10daf83c6dc6a5bcfa28
liujake <liuz@ucar.edu>
2022-12-08 12:59:50 -0700

    Enable the build of the latest code 	modified:   CMakeLists.txt 	modified:   env-setup/gnu-openmpi-cheyenne.csh 	modified:   env-setup/gnu-openmpi-cheyenne.sh 	modified:   env-setup/intel-impi-cheyenne.csh 	modified:   env-setup/intel-impi-cheyenne.sh

--- env-setup/gnu-openmpi-cheyenne.sh
+++ env-setup/gnu-openmpi-cheyenne.sh
@@ -1,12 +1,20 @@
-source /etc/profile.d/modules.sh
-export OPT=/glade/work/jedipara/cheyenne/opt/modules
+# contains atlas/0.30.0 from skylab-2.0.0-gnu-10.1.0
+#
+source /etc/profile.d/modules.sh  # note: needed on non-computing nodes
 module purge
-module use $OPT/modulefiles/core
-module load jedi/gnu-openmpi
-unalias ecbuild
-#export LOCAL_PATH_JEDI_TESTFILES=/glade/u/home/maryamao/JEDI_test_files
-git lfs install
+module unuse /glade/u/apps/ch/modulefiles/default/compilers
+export MODULEPATH_ROOT=/glade/work/jedipara/cheyenne/spack-stack/modulefiles
+module use /glade/work/jedipara/cheyenne/spack-stack/modulefiles/compilers
+module use /glade/work/jedipara/cheyenne/spack-stack/modulefiles/misc
+module load miniconda/3.9.12
+module load ecflow/5.8.4
+
 ulimit -s unlimited
-export OOPS_TRACE=0  # Note: Some ctests fail when OOPS_TRACE=1
-module list
 export GFORTRAN_CONVERT_UNIT='big_endian:101-200'
+module use /glade/work/jedipara/cheyenne/spack-stack/spack-stack-v1/envs/skylab-2.0.0-gnu-10.1.0/install/modulefiles/Core
+module load stack-gcc/10.1.0
+module load stack-openmpi/4.1.1
+module load stack-python/3.9.12
+#module available
+module load jedi-mpas-env/1.0.0
+module list
