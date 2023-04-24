609e6c8dc92d6af8dc7e0479359a0818606e302c
Jerahmie Radder <jerahmie@uwalumni.com>
2023-04-24 00:14:21 -0500

    Update env-setup scripts for cheyenne (#6)
    TYPE: bug fix
    
    KEYWORDS: cheyenne, environment, modules
    
    DESCRIPTION OF CHANGES: updated environment scripts for cheyenne to match those in JCSDA-internal repository.
    
    ISSUE:
    
    LIST OF MODIFIED FILES:
    M env-setup/gnu-openmpi-cheyenne.csh
    M env-setup/gnu-openmpi-cheyenne.sh
    M env-setup/intel-impi-cheyenne.csh
    M env-setup/intel-impi-cheyenne.sh
    
    TESTS CONDUCTED:
    Build/Ctest passed on Cheyenne.

--- env-setup/gnu-openmpi-cheyenne.sh
+++ env-setup/gnu-openmpi-cheyenne.sh
@@ -1,4 +1,4 @@
-# contains atlas/0.30.0 from skylab-2.0.0-gnu-10.1.0
+# contains atlas/0.31.1
 #
 source /etc/profile.d/modules.sh  # note: needed on non-computing nodes
 module purge
@@ -11,10 +11,9 @@ module load ecflow/5.8.4
 
 ulimit -s unlimited
 export GFORTRAN_CONVERT_UNIT='big_endian:101-200'
-module use /glade/work/jedipara/cheyenne/spack-stack/spack-stack-v1/envs/skylab-2.0.0-gnu-10.1.0/install/modulefiles/Core
+module use /glade/work/jedipara/cheyenne/spack-stack/spack-stack-v1/envs/skylab-3.0.0-gnu-10.1.0/install/modulefiles/Core
 module load stack-gcc/10.1.0
 module load stack-openmpi/4.1.1
 module load stack-python/3.9.12
-#module available
 module load jedi-mpas-env/1.0.0
 module list
--- env-setup/intel-impi-cheyenne.sh
+++ env-setup/intel-impi-cheyenne.sh
@@ -1,4 +1,4 @@
-# contains atlas/0.30.0 from skylab-2.0.0-gnu-10.1.0
+# contains atlas/0.31.1
 #
 source /etc/profile.d/modules.sh  # note: needed on non-computing nodes
 module purge
@@ -10,12 +10,10 @@ module load miniconda/3.9.12
 module load ecflow/5.8.4
 
 ulimit -s unlimited
-export GFORTRAN_CONVERT_UNIT='big_endian:101-200'
 
-module use /glade/work/jedipara/cheyenne/spack-stack/spack-stack-v1/envs/skylab-2.0.0-intel-19.1.1.217/install/modulefiles/Core
+module use /glade/work/jedipara/cheyenne/spack-stack/spack-stack-v1/envs/skylab-3.0.0-intel-19.1.1.217/install/modulefiles/Core
 module load stack-intel/19.1.1.217
 module load stack-intel-mpi/2019.7.217
 module load stack-python/3.9.12
-#module available
-module load jedi-mpas-env/1.0.0
+module load jedi-base-env/1.0.0 # this should be jedi-mpas-env/1.0.0, but module does not exist
 module list
