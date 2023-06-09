65ad83160c61d25d461b54fafec2a8597705c0f0
Zhiquan (Jake) Liu <liuz@ucar.edu>
2023-06-09 16:42:47 -0600

    Bring the develop branch build env to spack-stack-1.3.1 (#8)
    TYPE: maintenance
    
    KEYWORDS: Build, Cheyenne, spack-stack
    
    DESCRIPTION OF CHANGES:
    Bring the develop branch's build environment to spack-stack-1.3.1
    
    LIST OF MODIFIED FILES:
    M CMakeLists.txt
    M env-setup/gnu-openmpi-cheyenne.csh
    M env-setup/gnu-openmpi-cheyenne.sh
    M env-setup/intel-impi-cheyenne.csh
    M env-setup/intel-impi-cheyenne.sh
    
    TESTS CONDUCTED: Ctest on Cheyenne.

--- env-setup/gnu-openmpi-cheyenne.sh
+++ env-setup/gnu-openmpi-cheyenne.sh
@@ -1,19 +1,21 @@
-# contains atlas/0.31.1
+#
+echo "Loading Spack-Stack 1.3.1"
 #
 source /etc/profile.d/modules.sh  # note: needed on non-computing nodes
 module purge
-module unuse /glade/u/apps/ch/modulefiles/default/compilers
-export MODULEPATH_ROOT=/glade/work/jedipara/cheyenne/spack-stack/modulefiles
-module use /glade/work/jedipara/cheyenne/spack-stack/modulefiles/compilers
+export LMOD_TMOD_FIND_FIRST=yes
 module use /glade/work/jedipara/cheyenne/spack-stack/modulefiles/misc
 module load miniconda/3.9.12
 module load ecflow/5.8.4
+module load mysql/8.0.31
 
-ulimit -s unlimited
-export GFORTRAN_CONVERT_UNIT='big_endian:101-200'
-module use /glade/work/jedipara/cheyenne/spack-stack/spack-stack-v1/envs/skylab-3.0.0-gnu-10.1.0/install/modulefiles/Core
+module use /glade/work/epicufsrt/contrib/spack-stack/spack-stack-1.3.1/envs/unified-env/install/modulefiles/Core
 module load stack-gcc/10.1.0
 module load stack-openmpi/4.1.1
 module load stack-python/3.9.12
-module load jedi-mpas-env/1.0.0
+module load jedi-mpas-env/unified-dev
 module list
+
+ulimit -s unlimited
+export GFORTRAN_CONVERT_UNIT='big_endian:101-200'
+
